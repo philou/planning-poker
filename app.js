bbfe6fbf2f1124b16b1a297dc7057ecd74ba3fb8
philou <philippe.bourgau@gmail.com>
2016-06-30 06:01:53 +0200

    Make the docker configuration Heroku ready
    - add .gitignore for docker-compose.override files used for dev
    - update Dockerfile and docker-compose.yml for heroku
    - add heroku's Procile and app.js
    - configure the database to heroku settings

---
+++ app.js
@@ -0,0 +1,8 @@
+{
+  "name": "Planning Poker",
+  "description": "An app to do enable agile teams to do planning poker estimations remotely",
+  "image": "heroku/ruby",
+  "addons": [
+    "heroku-postgresql"
+  ]
+}

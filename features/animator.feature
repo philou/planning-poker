a2567b28f9363b1a6e35d50769069400a45c3691
philou <philippe.bourgau@gmail.com>
2016-07-21 08:23:50 +0200

    Scenario for first animator
    - write the scenario
    - implement the first step using Capybara

---
+++ features/animator.feature
@@ -0,0 +1,6 @@
+Feature: Animator
+
+  Scenario: The first to enter a team page becomes the animator
+
+    When Joe is the first to enter the team space
+    Then he becomes the animator
---
+++ features/step_definitions/homepage.rb
@@ -0,0 +1,10 @@
+When(/^Joe is the first to enter the team space$/) do
+
+  visit root_path
+
+  within("#Session") do
+    fill_in 'Team', :with => 'SuperTeam'
+    fill_in 'Name', :with => 'Joe'
+  end
+  click_button 'Enter'
+end

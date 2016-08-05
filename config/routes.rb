1204da748aa138612fbfe682797220d0ef64fa7c
philou <philippe.bourgau@gmail.com>
2016-08-05 19:41:21 +0200

    Forbid unused routes to TeamMember resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  resources :team_members
+  resources :team_members, only: [:create, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

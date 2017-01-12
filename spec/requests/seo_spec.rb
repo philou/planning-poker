cbb6d5b2fa02debc9d67dc516b99baf256b1cd5c
philou <philippe.bourgau@gmail.com>
2017-01-12 09:06:07 +0100

    Add a title in the layout
    - add a request spec to test the seo and the title of main page in particular
    - add a default title in the layout

---
+++ spec/requests/seo_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "static_pages/home.html.erb", type: :request do
+
+  it "The home page has the main title" do
+    get "/"
+
+    assert_select "title", "Philou's Planning Poker"
+  end
+end

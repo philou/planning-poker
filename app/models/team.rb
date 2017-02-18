5e6884a5d75ff1121320397c9209d7407b325528
philou <philippe.bourgau@gmail.com>
2017-02-18 12:06:31 +0100

    Add teaspoon task to default rake task
    - add lib/tasks/teaspoon.rake
    - update an annotation comment

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,7 +3,7 @@ class Team < ApplicationRecord
   belongs_to :animator, class_name: "TeamMember", optional: true
 
   def if_needed_pick_animator(team_member)
-    # TODO The optimistic lock is not great, they could still have race conditions when getting out of the role of animator, maybe emitting raw sql would be better (there is a sql ast builder in rails)
+    # TODO The optimistic lock is not great, they could still have race conditions when getting out of the role of animator, maybe emitting raw sql would be better (arel is an sql ast builder)
     begin
       if self.animator.nil?
         self.update(animator: team_member)

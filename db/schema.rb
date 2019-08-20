55aa4664e8ed7821e6baa7446395d448227be57c
Philippe Bourgau <philippe.bourgau@gmail.com>
2019-08-20 09:34:38 +0200

    Update dependencies to fix nokogiri vulnerability

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512070029) do
+ActiveRecord::Schema.define(version: 2017_05_12_070029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
